9b3c88b1d6b2b7d5702cd225cd1c7601ca394ef9
Your Name <mcpgco@gmail.com>
2022-06-24 22:31:47 -0700

    added code that doesnt work.. yet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api');
 
 
-router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>yowzaa that a 404 Error!</h1>');
